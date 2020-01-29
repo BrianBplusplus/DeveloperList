1af395ddf169f67461ea0522558bf80368f71af9
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-01-29 15:00:02 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import DevelopersList from "./components/DevelopersList";
+import Homepage from "./components/Homepage";
+import Toolbar from "./components/Toolbar";
+import PostPage from "./components/PostPage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <DevelopersList />
+        <Toolbar />
+        <Switch>
+          <Route path="/developers" component={DevelopersList} />
+          <Route path="/" component={Homepage} />
+        </Switch>
+        <Route path="/read/:id" component={PostPage} />
       </header>
     </div>
   );
--- src/components/DevelopersList.js
+++ src/components/DevelopersList.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import api from "../api";
 import Developer from "./Developer";
-import { developersFetched, fetchDevelopers } from "../store/developers/action";
+import { fetchDevelopers } from "../store/developers/action";
 
 // The "unconnected" inner component:
 class DevelopersList extends React.Component {
